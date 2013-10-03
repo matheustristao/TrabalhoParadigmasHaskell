c9fa71c9bb8a599a8fb66fcd0285a9e7988a0a93
Matheus Oliveira <matheus.tristao@gmail.com>
2013-10-03 00:47:09 -0300

    Alteração de pilha pra fila

--- Fila.hs
+++ Fila.hs
@@ -17,9 +17,9 @@ vazia(Fl a) = Fvazia
 push :: a -> Fila a -> Fila a
 push x (Fl y) = Fl(y ++ [x])	
 
-pop :: Fila a -> Fila a
+pop :: Fila a -> Fila b
 pop (Fl []) = error "Fila Vazia!!!!"
-pop (Fl(a:b)) = Fl b
+pop (Fl(a:b)) = Fl a
 
 top :: Fila a -> a
 top (Fl []) = error "Fila Vazia!!!!"
