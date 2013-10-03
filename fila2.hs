f2e78cbae8e611d424b76569f82128a723829f50
anapaulavargas <anapaulavnoronha@gmail.com>
2013-10-03 09:29:13 -0300

    Fila - funcao que verifica se a fila esta vazia e retorna o primeiro elemento da fila
    funcao que verifica se a fila esta vazia e retorna o primeiro elemento
    da fila

--- fila2.hs
+++ fila2.hs
@@ -1,6 +1,8 @@
-module (Queue, enqueue) where
+module (Queue, enqueue, queueEmpty, front) where
 enqueue :: t -> Queue t -> Queue t
 dequeue :: Queue t-> Queue t
+queueEmpty :: Queue t -> Bool
+front :: Queue t -> t
 
 data Queue t = Fila [t]
 
@@ -8,3 +10,9 @@ enqueue x (Fila q) = Fila (q ++ [x]) -- insere o elemento no final da lista
 
 dequeue (Fila(x:xs)) = Fila xs -- tira o elemento da cabeca da lista
 dequeue _ = error "Fila de espera vazia"
+
+queueEmpty (Fila[]) = True
+queueEmpty _ = False
+
+front (Fila (x : _) = x -- o front eh o elemento a ser retirado
+front _ = error "Fila vazia"
